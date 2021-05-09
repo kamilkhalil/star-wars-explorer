54dbbcce7d3cf8b5ec62d8efffe0d3b247b055da
Kamil Khalil <kamil.khalil91@gmail.com>
2021-05-09 13:46:42 +0200

    initial version of filtering data

--- swapi_collections/views.py
+++ swapi_collections/views.py
@@ -15,13 +15,25 @@ def index(request):
     return render(request, "swapi_collections/index.html", {"collections": char_collections})
 
 
-def collection_detail(request, id): #TODO: validation of get parameters
+def collection_detail(request, id): #TODO: validation of parameters
     collection = get_object_or_404(Collection, pk=id)
-    print(request.POST)
-    if request.GET.get("load_button") == "Load More":
-        elem_num = int(request.GET.get("rows_count",  0)) + 11
-    else:
-        elem_num = 11
     table = petl.fromcsv(get_file_path(collection.filename.name))
-    rows = list(dict(zip(petl.header(table), x)) for x in table[1:elem_num])
-    return render(request, "swapi_collections/collection.html", {"file_name": collection.filename, "rows": rows})
+    if request.method == "GET":
+        if request.session.get('filter') is not None :
+            print(f"Tu jestem {request.session.get('filter')}")
+            table = petl.valuecounts(table, *request.session.get("filter"))
+            del request.session['filter']
+        if request.GET.get("load_button") == "Load More":
+            elem_num = int(request.GET.get("rows_count",  0)) + 11
+        else:
+            elem_num = 11
+        rows = list(dict(zip(petl.header(table), x)) for x in table[1:elem_num])
+        return render(request, "swapi_collections/collection.html", {"file_name": collection.filename, "rows": rows})
+    if request.method == "POST":
+        print(request.POST)
+        if request.POST.get("on") is not None:
+            request.session['filter'] = request.POST.getlist("on")
+        return redirect("collection", id)
+
+            
+
